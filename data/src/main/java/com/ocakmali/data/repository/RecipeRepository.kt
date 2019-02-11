7baced9d0265eb84437cacb0122428aa438b7398
Mehmet Ali Ocak <ocakmali@gmail.com>
2019-02-11 17:13:07 +0300

    create recipe and timestamp repositories

---
+++ data/src/main/java/com/ocakmali/data/repository/RecipeRepository.kt
@@ -0,0 +1,67 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Mehmet Ali Ocak
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.ocakmali.data.repository
+
+import com.ocakmali.common.DispatchersProvider
+import com.ocakmali.common.Result
+import com.ocakmali.data.dao.RecipeDao
+import com.ocakmali.data.dao.RecipeTimestampDao
+import com.ocakmali.data.entity.toEntity
+import com.ocakmali.data.entity.toRecipe
+import com.ocakmali.domain.model.Recipe
+import com.ocakmali.domain.model.RecipeTimestamp
+import com.ocakmali.domain.repository.IRecipeRepository
+import kotlinx.coroutines.withContext
+
+class RecipeRepository(private val recipeDao: RecipeDao,
+                       private val timestampDao: RecipeTimestampDao,
+                       private val dispatchers: DispatchersProvider) : IRecipeRepository {
+
+    override fun loadRecipes() = recipeDao.loadRecipes().map { it.toRecipe() }
+
+    override suspend fun addRecipe(recipe: Recipe) = withContext(dispatchers.io) {
+        Result.value { recipeDao.insert(recipe.toEntity()) }
+    }
+
+    override suspend fun addRecipeAndTimestamps(recipe: Recipe, timestamps: List<RecipeTimestamp>) = withContext(dispatchers.io) {
+        val result = Result.value { recipeDao.addRecipe(recipe.toEntity()) }
+        return@withContext when(result) {
+            is Result.Error -> Result.buildError(result.error)
+            is Result.Value -> Result.value {
+                timestampDao.insert(timestamps.map {
+                    it.copy(recipeId = result.value.toInt()).toEntity()
+                })
+            }
+        }
+    }
+
+    override suspend fun addRecipes(recipes: List<Recipe>) = withContext(dispatchers.io) {
+        Result.value { recipeDao.insert(recipes.map { it.toEntity() }) }
+    }
+
+    override suspend fun deleteRecipe(recipe: Recipe) = withContext(dispatchers.io) {
+        Result.value { recipeDao.delete(recipe.toEntity()) }
+    }
+}
