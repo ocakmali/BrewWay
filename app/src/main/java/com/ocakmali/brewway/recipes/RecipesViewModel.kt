35b22f03fbfbfc8148de68e737a2a48fc25a7008
Mehmet Ali Ocak <ocakmali@gmail.com>
2019-02-14 14:25:04 +0300

    create recipes views

---
+++ app/src/main/java/com/ocakmali/brewway/recipes/RecipesViewModel.kt
@@ -0,0 +1,39 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Mehmet Ali Ocak
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.ocakmali.brewway.recipes
+
+import androidx.paging.toLiveData
+import com.ocakmali.brewway.base.BaseViewModel
+import com.ocakmali.brewway.datamodel.toView
+import com.ocakmali.common.DispatchersProvider
+import com.ocakmali.domain.interactor.RecipeInterActor
+
+class RecipesViewModel(private val interActor: RecipeInterActor,
+                       dispatchers: DispatchersProvider) : BaseViewModel(dispatchers) {
+
+    val recipes = interActor.loadRecipes()
+            .map { it.toView() }
+            .toLiveData(20)
+}
