820d9ecca4c1e60d00d906d7fbf94e76c19b94e8
Mehmet Ali Ocak <ocakmali@gmail.com>
2019-03-17 12:38:58 +0300

    Created SimpleTextWatcher

---
+++ app/src/main/java/com/ocakmali/brewway/view/widget/SimpleTextWatcher.kt
@@ -0,0 +1,39 @@
+package com.ocakmali.brewway.view.widget
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+
+class SimpleTextWatcher(private val editText: EditText,
+                        private val send: (String) -> Unit)
+    : TextWatcher, LifecycleObserver {
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun attach() {
+        editText.addTextChangedListener(this)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun detach() {
+        editText.removeTextChangedListener(this)
+    }
+
+    fun registerLifecycle(lifecycle: Lifecycle) {
+        lifecycle.addObserver(this)
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+        s?.let {
+            send(s.toString())
+        }
+    }
+}
