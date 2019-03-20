dc39b56aeb59e031f3ee7938444a5d9befd2fafa
Mehmet Ali Ocak <ocakmali@gmail.com>
2019-03-20 17:54:38 +0300

    Created DynamicAutoCompleteTextView

---
+++ app/src/main/java/com/ocakmali/brewway/view/widget/DynamicAutoCompleteTextView.kt
@@ -0,0 +1,39 @@
+package com.ocakmali.brewway.view.widget
+
+import android.content.Context
+import android.graphics.Rect
+import android.text.Editable
+import android.util.AttributeSet
+import androidx.appcompat.R
+import androidx.appcompat.widget.AppCompatAutoCompleteTextView
+import androidx.lifecycle.Lifecycle
+
+class DynamicAutoCompleteTextView @JvmOverloads constructor(context: Context,
+                                                            attrs: AttributeSet? = null,
+                                                            defStyleRes: Int = R.attr.autoCompleteTextViewStyle)
+    : AppCompatAutoCompleteTextView(context,attrs,defStyleRes) {
+
+    private var textWatcher: LifecycleTextWatcher? = null
+
+    init {
+        threshold = 1
+    }
+
+    fun setTextWatcherAndRegisterLifecycle(lifecycle: Lifecycle, onTextChange: (Editable) -> Unit) {
+        setTextWatcher(onTextChange)
+        registerLifecycle(lifecycle)
+    }
+
+    fun setTextWatcher(onTextChange: (Editable) -> Unit) {
+        textWatcher = LifecycleTextWatcher(this, onTextChange)
+    }
+
+    fun registerLifecycle(lifecycle: Lifecycle) {
+        textWatcher?.registerLifecycle(lifecycle)
+    }
+
+    override fun onFocusChanged(focused: Boolean, direction: Int, previouslyFocusedRect: Rect?) {
+        super.onFocusChanged(focused, direction, previouslyFocusedRect)
+        if (focused) showDropDown()
+    }
+}
