e406ff2f602631ad31e4a0aea60cf0e33fd8313e
Mehmet Ali Ocak <ocakmali@gmail.com>
2019-05-06 23:43:46 +0300

    Created BaseFragment

--- app/src/main/java/com/ocakmali/brewway/equipments/coffees/CoffeesFragment.kt
+++ app/src/main/java/com/ocakmali/brewway/equipments/coffees/CoffeesFragment.kt
@@ -1,13 +1,10 @@
 package com.ocakmali.brewway.equipments.coffees
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearSnapHelper
 import com.ocakmali.brewway.R
+import com.ocakmali.brewway.base.BaseFragment
 import com.ocakmali.brewway.datamodel.CoffeeView
 import com.ocakmali.brewway.exceptions.EmptyItem
 import com.ocakmali.brewway.extensions.setOnActionDoneClickListener
@@ -16,13 +13,11 @@ import kotlinx.android.synthetic.main.fragment_coffees.*
 import kotlinx.android.synthetic.main.layout_equipments_grid_recycler.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class CoffeesFragment : Fragment() {
+class CoffeesFragment : BaseFragment() {
 
-    private val viewModel: CoffeesViewModel by viewModel()
+    private val viewModel by viewModel<CoffeesViewModel>()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_coffees, container, false)
-    }
+    override fun layoutResId(): Int = R.layout.fragment_coffees
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
--- app/src/main/java/com/ocakmali/brewway/recipes/RecipesFragment.kt
+++ app/src/main/java/com/ocakmali/brewway/recipes/RecipesFragment.kt
@@ -25,25 +25,20 @@
 package com.ocakmali.brewway.recipes
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ocakmali.brewway.R
+import com.ocakmali.brewway.base.BaseFragment
 import kotlinx.android.synthetic.main.fragment_recipes.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class RecipesFragment : Fragment() {
+class RecipesFragment : BaseFragment() {
 
     private val viewModel: RecipesViewModel by viewModel()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
-    }
+    override fun layoutResId() = R.layout.fragment_recipes
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
